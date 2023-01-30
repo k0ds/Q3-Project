92695390a959c539e938a6fe38f9f5a33efda94a
k0ds <cathating@protonmail.com>
2023-01-30 17:13:19 -0500

    added more functions

--- AV.py
+++ AV.py
@@ -4,8 +4,8 @@ class AbsoluteValue(Scene):
     def construct(self):
 
         ax = Axes(
-            x_range=[0, 10, 1],
-            y_range=[-8, 10, 2],
+            x_range=[-5, 5, 1],
+            y_range=[-5, 3, 1],
             tips=False,
             axis_config={"include_numbers": True, "font_size": 30}
          )
@@ -17,19 +17,43 @@ class AbsoluteValue(Scene):
         AbValue.set_color_by_tex("k", color=BLUE)
 
 
+        formexp = Text("a tells us how far the graph stretches vertically and if it opens up or down,\n h and k tells us how far the graph shifts horizontally and vertically",font_size=25, t2c={'[:1]':ORANGE, '[5:7]':BLUE, 'k':BLUE}).to_edge(DOWN)
+        
+        
+
+
+
 
         AbValueText =  Text("The absolute value equation has the form: ", font_size=25, color=YELLOW).to_edge(UP)
+        parentfunctext = Text("The parent function: ", font_size=25, color=WHITE).to_edge(DR + UP)
+        parentfunc = MathTex("y = |x|").next_to(parentfunctext, DOWN)
+
 
         self.play(FadeIn(AbValueText))
         self.play(Create(AbValue))
+        self.play(FadeIn(formexp))
         self.wait(3)
 
         self.play(FadeOut(AbValue, AbValueText))
 
-        #−2∣x∣+4
+
                                 #replace this with a real ab value func
-        graph = ax.plot(lambda x: -2 * x + 4 , x_range=[0,9], use_smoothing=False, color = YELLOW)
+        graph = ax.plot(lambda x: np.abs(x) , x_range=[-5,5], use_smoothing=False, color=YELLOW)
+
 
 
         self.play(Create(ax))
         self.play(Create(graph))
+        self.play(FadeIn(parentfunctext))
+        self.play(FadeIn(parentfunc))
+
+        self.wait(3)
+
+        Dialoague1 = Text("If we wanted to graph this function: ", font_size=25, color=YELLOW)
+
+        func2 = MathTex(r"f(x)= -2 |x| + 4", substrings_to_isolate="-2, x, 4").next_to(Dialoague1,DOWN)
+
+
+
+
+
