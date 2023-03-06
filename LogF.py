0db796e6bed6dcd695d5288f15eef9aa06ae4516
k0ds <cathating@protonmail.com>
2023-03-06 18:47:38 -0500

    basis

--- LogF.py
+++ LogF.py
@@ -1,1 +1,15 @@
 #for example use decibel scale as its log
+from manim import *
+
+class Logarithmic(Scene):
+    def construct(self):
+
+        ax = Axes(
+            x_range=[-5, 5, 1],
+            y_range=[-1, 10, 1],
+            tips=False,
+            axis_config={"include_numbers": True, "font_size": 30}
+        )
+
+
+
