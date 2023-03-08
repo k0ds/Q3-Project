a5b447a13db52ddc3d92c5e75884b1e2a6bbcf5d
k0dsdev <cathating@protonmail.com>
2023-03-07 23:51:08 -0500

    more stuff. close to done

--- LogF.py
+++ LogF.py
@@ -6,8 +6,8 @@ class Logarithmic(Scene):
     def construct(self):
 
         ax = Axes(
-            x_range=[-1, 10, 2],
-            y_range=[-10, 5, 2],
+            x_range=[-3, 8, 1],
+            y_range=[-5, 4, 1],
             tips=False,
             axis_config={"include_numbers": True, "font_size": 30}
         )
@@ -17,10 +17,12 @@ class Logarithmic(Scene):
         parent2 = MathTex(r"f(x) = log_{2} x").to_corner(UL)
         parent.set_color_by_tex("a", ORANGE)
         parent.set_color_by_tex("x", YELLOW)
-        domain = Text("Domain: (0, ++∞)", font_size=15).shift(RIGHT + DOWN)
-        rangee = Text("Range: ℝ", font_size=15)
-        funfact = Text("The logarithmic function is the inverse of the exponential function!", font_size=20, color=ORANGE).to_edge(DOWN)
-        u1 = Underline(funfact[20:27])
+        domain = Text("Domain: (0, +∞)", font_size=18).to_corner(DR)
+        rangee = Text("Range: ℝ", font_size=18).next_to(domain, UP)
+        funfact = Text("The logarithmic function is the inverse of the exponential function!", font_size=25, color=ORANGE)
+        u1 = Underline(funfact[27:34], color=ORANGE)
+
+        asymp = Text("Notice that f(x) never touches -y,\n but approaches asymptotically as  y → -∞", font_size=16, t2c={'[50:65]': ORANGE}, color=WHITE).next_to(domain, UP + LEFT*3)
 
         self.play(FadeIn(parenttext))
         self.play(FadeIn(parent))
@@ -29,11 +31,51 @@ class Logarithmic(Scene):
         self.play(parent.animate.scale(0.8))
         self.play(parent.animate.to_corner(UL))
         self.play(FadeIn(ax))
-        graph = ax.plot(lambda x: np.log2(x), use_smoothing=False, color=YELLOW)
+        graph = ax.plot(lambda x: math.log2(x),x_range=[0.00001, 10] ,use_smoothing=False, color=YELLOW)
         self.play(Transform(parent, parent2))
-        self.play(Create(graph))
+        self.play(Write(graph))
         self.wait(2)
-        #self.play(FadeIn(domain, rangee))
+        self.play(FadeIn(domain, rangee, asymp))
+        self.wait(3)
+
+        self.play(FadeOut(domain, rangee, asymp, ax, graph, parent))
+
+        self.play(FadeIn(funfact, u1))
+        funfactg = Group(funfact, u1)
+        self.play(funfactg.animate.scale(.6))
+        self.play(funfactg.animate.to_corner(UL))
+
+        ax2 = Axes(
+            x_range=[-5, 10, 1],
+            y_range=[-5, 10, 1],
+            tips=False,
+            axis_config={"include_numbers": True, "font_size": 30}
+        )
+
+        graph2 = ax2.plot(lambda x: 2.718**x, x_range=[-5,9], use_smoothing=False, color = RED)
+        graph = ax2.plot(lambda x: math.log(x),x_range=[0.00001, 10] ,use_smoothing=False, color=YELLOW)    
+        line = ax2.plot(lambda x: x, use_smoothing=False, color=WHITE)
+        log1 = MathTex(r"y = log_{e} ^x", color=YELLOW).next_to(funfact, DOWN)
+        exp1 = MathTex(r"y = e^x", color=RED).next_to(log1, DOWN)
+        line1 = MathTex(r"y = x", color=WHITE).next_to(exp1, DOWN)
+        funcs = VGroup(log1, exp1, line1)
+        funcs.shift(LEFT)
+        log1.scale(.7)
+        exp1.scale(.7)
+        line1.scale(.7)
+
+        self.play(FadeIn(ax2))
+        self.play(Write(graph))
+        self.play(Write(graph2))
+        self.play(Write(line))
+        self.play(FadeIn(funcs))
+        self.wait(3)
+
+        self.play(FadeOut(funcs, ax2, graph, graph2, line, funfactg))
+        
+
+
+       
 
 
 
